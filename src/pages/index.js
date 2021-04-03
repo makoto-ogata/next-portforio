4469d5a825d845daea80fd460beadc847544395d
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-04-03 11:03:27 +0900

    added link tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 import * as style from '../styles/index.module.css';
 
 const Index =()=> {
   return(
-    <h1 className={style.h1Text}>こんにちは</h1>
+    <div>
+      <h1 className={style.h1Text}>こんにちは</h1>
+      <Link href="/contact"><a>Contactページへ移動</a></Link>
+    </div>
   )
 }
 
