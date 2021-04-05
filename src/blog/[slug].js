eed8ee0c099ca4e4855d285f182e77be339a93cf
Makoto Ogata <ogatamakoto1976@gmail.com>
2021-04-06 00:07:32 +0900

    Edit [slug].js and blog.js

--- src/blog/[slug].js
+++
@@ -1,8 +0,0 @@
-
-const SingleBlog =()=> {
-  return (
-    <h1>記事ページ</h1>
-  )
-}
-
-export default SingleBlog;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import matter from 'gray-matter';
 import Link from 'next/link';
 
 const Blog =(props)=> {
-  console.log(props);
+  // console.log(props);
   return (
     <div>
       <h1>ブログページ</h1>
@@ -21,24 +21,30 @@ export default Blog;
 
 export async function getStaticProps() {
   const blogs = ((context) => {
-    const keys = context.keys();
-    const values = keys.map(context);
+    const keys = context.keys()
+    const values = keys.map(context)
 
     const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3);
-      const value = values[index];
+      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
+      const value = values[index]
       const document = matter(value.default)
       return {
         frontmatter: document.data,
         slug: slug
       }
     })
-    return data;
+    return data
 
   })(require.context('../data', true, /\.md$/))
+
+  const orderedBlogs = blogs.sort((a, b) => {
+    return b.frontmatter.id - a.frontmatter.id
+  })
+
+
   return {
     props: {
-      blogs: JSON.parse(JSON.stringify(blogs))
+      blogs: JSON.parse(JSON.stringify(orderedBlogs))
     },
   }
 }
